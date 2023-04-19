eacbfee142d5896a839bd73163527b268857e6dd
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2023-04-19 15:28:51 -0300

    modified:   index.html 	new file:   public/favicon.svg 	deleted:    public/vite.svg 	modified:   src/App.tsx 	new file:   src/assets/atletas.svg 	deleted:    src/assets/favicon.svg 	new file:   src/assets/iniciantes.svg 	new file:   src/assets/praticantes.svg 	new file:   src/assets/right-arrow.svg 	new file:   src/assets/strongA.svg 	modified:   src/components/Foot.tsx 	modified:   src/components/Head.tsx 	modified:   src/components/Main.tsx

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,19 +1,22 @@
-import React from "react";
 import Background from "../assets/background1.svg"
 import Logo from "../assets/logo.svg"
+
+import Iniciantes from "../assets/iniciantes.svg"
+import Praticantes from "../assets/praticantes.svg"
+import Atletas from "../assets/atletas.svg"
 
 export default function Head() {
     return (
         <section className=
-            "flex flex-col min-h-screen min-w-screen justify-between bg-no-repeat bg-right-top"
-        style={{
-            backgroundImage: `url(${Background})`,
-            backgroundSize: "795px",
-        }}>
-            <div className="flex flex-col p-20 space-y-8">
-                <img src={Logo} className="w-48 h-16"/>
+            "flex flex-col min-h-screen min-w-screen">
+            <div className=
+            "flex flex-col h-2/3 px-24 py-16 space-y-8 bg-contain bg-no-repeat bg-right border-b-2 border-red"
+            style=
+                {{backgroundImage: `url(${Background})`
+            }}>
+                <img src={Logo} className="w-40 h-16"/>
 
-                <h1 className="text-4xl font-bold w-1/2">Transforme o seu corpo
+                <h1 className="text-3xl font-bold w-1/2">Transforme o seu corpo
                     <b className="text-red"> 5x </b>
                     mais
                     <b className="text-red"> rápido</b>
@@ -29,8 +32,25 @@ export default function Head() {
                 </button>
             </div>
 
-            <div className="flex flex-col">
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt hic quasi dolorum error unde quibusdam. <br/> Animi doloribus fuga consequuntur dolore eligendi saepe. Perferendis ad modi officiis doloribus non eligendi?</h1>
+            <div className="flex flex-col justify-evenly items-center h-72 bg-slate border-b-2 border-red">
+                <h1 className="uppercase font-bold text-2xl">Feito para você</h1>
+
+                <div className="flex space-x-24">
+                    <div className="flex flex-col items-center space-y-2">
+                        <img src={Iniciantes} className='h-16'/>
+                        <span className="text-center w-64"> <b>Iniciante</b> na busca do corpo perfeito</span>
+                    </div>
+
+                    <div className="flex flex-col items-center space-y-2">
+                        <img src={Praticantes} className='h-16'/>
+                        <span className="text-center w-64"> <b>Praticante</b> que não conseguem mais evoluir</span>
+                    </div>
+
+                    <div className="flex flex-col items-center space-y-2">
+                        <img src={Atletas} className='h-16'/>
+                        <span className="text-center w-64"> <b>Atleta</b> em evolução e em competições fisiculturistas</span>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import strongA from "../assets/strongA.svg"
+import rightArrow from "../assets/right-arrow.svg"
 
 export default function Main() {
     return (
-        <div>
-            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, perferendis nesciunt animi, hic eveniet asperiores saepe veritatis iure laboriosam odit explicabo dolore quaerat ut reiciendis veniam eos repellendus accusantium aspernatur!</h1>
-            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, perferendis nesciunt animi, hic eveniet asperiores saepe veritatis iure laboriosam odit explicabo dolore quaerat ut reiciendis veniam eos repellendus accusantium aspernatur!</h1>
-            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, perferendis nesciunt animi, hic eveniet asperiores saepe veritatis iure laboriosam odit explicabo dolore quaerat ut reiciendis veniam eos repellendus accusantium aspernatur!</h1>
-        </div>
+        <section className="min-h-screen min-w-screen items-center">
+            <div className="flex flex-col items-center py-12 px-6">
+                <h1 className="uppercase font-bold text-3xl">
+                    Como funciona o
+                    <span className="text-red ml-1.5">
+                        método 5x
+                    </span>
+                </h1>
+
+                <div className="flex space-x-4">
+                    <div className="flex flex-col w-72 justify-end items-center uppercase font-bold bg-auto" style={{backgroundImage:`url: ${strongA}`}}>
+                        <div className="flex flex-col h-[24rem] pb-4 space-y-2 justify-end items-center border border-red text-center rounded-sm">
+                            <p className="text-lg">
+                                Transformando a alimentação
+                            </p>
+
+                            <span className="py-1 px-12 bg-crimsom rounded-full">
+                                Módulo 1
+                            </span>
+                        </div>
+                        
+                        <a href="#" className="flex w-72 space-x-1.5 items-center text-sm p-2 bg-crimsom">
+                            <img src={rightArrow}/>
+                            <span>
+                                Saiba Mais
+                            </span>
+                        </a>
+                    </div>
+                </div>
+
+                <button className="uppercase">
+                    Botão
+                </button>
+            </div>
+        </section>
     )
 }
